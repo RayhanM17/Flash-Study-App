0967ba74966406178cfe577821cf407c3221b35a
RayhanM17 <raysdigitalservice@gmail.com>
2022-12-24 01:09:28 -0500

    production

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -14,6 +15,19 @@ app.use(function(req, res, next) {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 
+// Routes
 app.use('/openai', require('./routes/openaiRoutes'))
+
+// Serve Frontend
+if(process.env.NODE_ENV === 'production') {
+  // Set build folder as static
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+  app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
+} else {
+  app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Welcome to the Support Desk API'})
+  })
+}
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
