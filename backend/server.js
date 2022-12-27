1e6031926fbd1c0b684b8c585900c67a9a245213
rayhanm17 <marrero.rayhan@gmail.com>
2022-12-26 20:48:28 -0500

    hosting

--- backend/server.js
+++ backend/server.js
@@ -6,15 +6,15 @@ const baseURL = process.env.BASE_URL || "http://localhost:3000"
 
 const app = express();
 
-// Enable body parser
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", baseURL); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
+
+// CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', baseURL);
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 // Routes
 app.use('/openai', require('./routes/openaiRoutes'))
@@ -27,7 +27,7 @@ if(process.env.NODE_ENV === 'production') {
   app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
 } else {
   app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Welcome to the Support Desk API'})
+    res.status(200).json({ message: 'Welcome to FlashStudy API'})
   })
 }
 
